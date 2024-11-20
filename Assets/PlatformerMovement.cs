3ab782c3fd1a6925e0b9af3b76cd940e43779c1b
Techbow909 <144063946+Techbow909@users.noreply.github.com>
2024-11-20 12:47:50 -0500

    dsad

--- Assets/PlatformerMovement.cs
+++ Assets/PlatformerMovement.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Ali Jawad Parpia
+
+// Ali Jawad Parpia
 public class PlatformerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
@@ -10,41 +11,54 @@ public class PlatformerMovement : MonoBehaviour
     private bool isJumping = false;
 
     public int jumpcounter = 0;
+    public Animator animator;
+    float horizontalMovement = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();
-      
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-
+        horizontalMovement = horizontalInput * moveSpeed;
         Vector2 moveVector = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
 
-    if (Input.GetButtonDown("Jump") && jumpcounter<2){
-        moveVector.y = jumpForce;
-        isJumping = true;
-        jumpcounter++;
+        // Handle jumping
+        if (Input.GetButtonDown("Jump") && jumpcounter < 2)
+        {
+            moveVector.y = jumpForce;
+            isJumping = true;
+            jumpcounter++;
+            animator.SetBool("isJumping", true);
+        }
+
+        rb.velocity = moveVector;
+
+        // Flip character sprite based on direction
+        if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
-    rb.velocity = moveVector;
-
-    if (horizontalInput < 0){
-        transform.localScale = new Vector3(-1, 1, 1);
-    }else if(horizontalInput > 0){
-        transform.localScale = new Vector3(1, 1, 1);
-    }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Ground")){
+    // This function is called when the object collides with something
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             isJumping = false;
             jumpcounter = 0;
+            animator.SetBool("isJumping", false);
         }
     }
 }
